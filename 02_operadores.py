aaa42d25766d548385ac7247f8cf5a9c75616f91
lucasDMadrigal <lucasmadrigalb@gmail.com>
2023-01-17 01:08:22 -0300

    hasta operadores logicos hs2:20

--- 02_operadores.py
+++ 02_operadores.py
@@ -12,3 +12,15 @@ print(10%3)
 print(10//3) ## da como resultado un numero entero
 print(2**3) ## operaciones de potencia
 
+# LAS SIGUIENTES EXPRESIONES DAN FALSE
+print("hola" == "bola")
+print("hola" == "zola")
+print("hola" == "HOLA")
+
+# Operadores logicos
+
+## Todas estas expresiones dan como resultado TRUE
+
+print(4 == (2*2) and 4*3 == 6+6)
+print(4 == (2*2) or 4*3 == 6+5)
+print(not(4 == (2*2) and 4*3 == 6+5))
