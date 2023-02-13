7e01fa8f2326973538b134426f09b6b7cfe2400b
lucasDMadrigal <lucasmadrigalb@gmail.com>
2023-02-13 18:35:59 -0300

    leccion 09

---
+++ 09_loops.py
@@ -0,0 +1,17 @@
+### loops ###
+
+# while
+
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+    my_condition += 1
+else:
+    print("my_condition es mayor o igual a 10")
+
+
+
+# while my_condition < 10:
+#     print(my_condition)
+#     my_condition += 1
